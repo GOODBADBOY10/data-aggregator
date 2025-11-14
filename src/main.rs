6800f05b9a455aaff37b2833ab0abdba7b272f7d
Goodbadboy <selfmanademola@gmail.com>
2025-11-14 12:45:29 +0100

    feat: add initial setup for Axum server with basic routes
    - Updated Cargo.toml to include dependencies for Axum, Tokio, Reqwest, Serde, and Serde JSON.
    - Implemented a basic Axum server in main.rs with two routes: root ("/") and "/response".
    - Defined data structures for Dexscreener, Coingecko, and Insidex to handle JSON serialization/deserialization.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Dexscreener {
+    name: String,
+    mcap: f64,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Coingecko {
+    price: u64,
+    symbol: String,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Insidex {
+    liquidity: f64,
+    trending: Vec<u64>,
+}
+
+
+
+#[tokio::main]
+async fn main() {
+    println!("Entry of thr main file");
+
+    let port = "0.0.0.0:3000";
+
+
+    let app = Router::new()
+    .route("/", get(get_root))
+    .route("/response", get(get_response));
+
+    let listener = tokio::net::TcpListener::bind(port).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+
+
+async fn get_root() -> &'static str {
+    println!("Hello from root");
+    "Root endpoint hit"
+}
+
+
+async fn get_response() {}
